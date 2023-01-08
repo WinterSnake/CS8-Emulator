94d64733737ed89d8a581d0c7a9311eee42a98a0
Winter_Snake <blizzardsnake98@gmail.com>
2023-01-07 21:01:29 -0500

    add: graphics window and cpu draw window

--- Chip8.Emulator.NCurses/Program.cs
+++ Chip8.Emulator.NCurses/Program.cs
@@ -10,6 +10,7 @@ using System;
 using System.IO;
 using System.Linq;
 using Sharpie;
+using Sharpie.Abstractions;
 using Sharpie.Backend;
 using Chip8 = Emulators.Chip8;
 
@@ -28,18 +29,35 @@ internal class Program
             Console.Error.WriteLine($"File '{args[0]}' does not exist or is not a file.");
             return;
         }
-        // Parse additional arguments (single-step, debug)
+        // TODO: Parse additional arguments (single-step, debug)
         bool singleStep = true;
         bool drawCPU = true;
         // Setup ncurses
         using var terminal = new Terminal(
             CursesBackend.Load(), new TerminalOptions(
-                UseMouse: false, CaretMode: CaretMode.Invisible
+                UseMouse: false, CaretMode: CaretMode.Invisible, ManagedWindows: true
             )
         );
+        var graphicsWindow = terminal.Screen.Window(new(0, 0, 66, 33));
+        var cpuWindow = terminal.Screen.Window(new(graphicsWindow.Size.Width + 1, 0, 41, graphicsWindow.Size.Height));
         // Create Chip8
         var Chip8 = new Chip8::Console();
         Chip8.LoadROM(args[0]);
+        // Draw
+        graphicsWindow.ColorMixture = terminal.Colors.MixColors(StandardColor.White, StandardColor.Black);
+        graphicsWindow.DrawBorder();
+        graphicsWindow.ColorMixture = terminal.Colors.MixColors(StandardColor.White, StandardColor.Red);
+        graphicsWindow.CaretLocation = new(1, 1);
+        for (var y = 0; y < 32; ++y)
+        {
+            for (var x = 0; x < 64; ++x)
+            {
+                graphicsWindow.WriteText(" ");
+            }
+            graphicsWindow.CaretLocation = new(1, y + 1);
+        }
+        terminal.Screen.Refresh();
+        graphicsWindow.Refresh();
         // Event handler
         foreach (var @event in terminal.Events.Listen(terminal.Screen))
         {
@@ -49,11 +67,11 @@ internal class Program
             else if (@event is KeyEvent { Char.Value: 'C', Modifiers: ModifierKey.Ctrl })
                 break;
             // Single Step Debugging
-            else if (@event is KeyEvent { Char.Value: 'N' } && singleStep) {}
+            else if (@event is KeyEvent { Char.Value: 'n' } && singleStep) { Chip8.Tick(); UpdateCPUDraw(Chip8, cpuWindow); }
             // Manual CPU Draw Update
-            else if (@event is KeyEvent { Char.Value: 'U' } && drawCPU) {}
+            else if (@event is KeyEvent { Char.Value: 'u' } && drawCPU) { UpdateCPUDraw(Chip8, cpuWindow); }
             // Handle Inputs
-            else if (@event is KeyEvent)
+            else if (@event is KeyEvent { Modifiers: ModifierKey.None })
             {
                 char key = Char.ToUpper((char)(@event as KeyEvent).Char.Value);
                 if (Program.KeyInputs.Contains(key))
@@ -63,6 +81,61 @@ internal class Program
                 }
             }
         }
+    }
+    public static void UpdateCPUDraw(Chip8::Console console, ITerminalSurface surface)
+    {
+        surface.CaretLocation = new(0, 0);
+        surface.DeleteLines(surface.Size.Height);
+        // Program Counter + OpCode
+        var instruction = (console.Memory[console.CPU.ProgramCounter] << 8) | console.Memory[console.CPU.ProgramCounter + 1];
+        surface.WriteText($"PC:0x{console.CPU.ProgramCounter.ToString("X4")} ");
+        surface.WriteText($"| OpCode: 0x{instruction.ToString("X4")} ");
+        surface.WriteText($"| I: 0x{console.CPU.AddressPointer.ToString("X4")}");
+        surface.CaretLocation = new(0, surface.CaretLocation.Y + 1);
+        // Stack
+        surface.WriteText("Stack: [");
+        for (var i = 0; i < console.CPU.Stack.Length; ++i)
+        {
+            // Empty Stack
+            if (console.CPU.Stack[i] == 0)
+                break;
+            // Handle initial breakpoint
+            if (i == 0)
+                surface.CaretLocation = new(1, surface.CaretLocation.Y + 1);
+            // Handle drawing stack
+            surface.WriteText($"0x{console.CPU.Stack[i].ToString("X4")}");
+            if (i < console.CPU.StackPointer - 1 && (i + 1) % 4 != 0)
+                surface.WriteText(", ");
+            else
+                surface.CaretLocation = new(i == console.CPU.StackPointer - 1 ? 0 : 1, surface.CaretLocation.Y + 1);
+        }
+        surface.WriteText("]");
+        surface.CaretLocation = new(0, surface.CaretLocation.Y + 1);
+        // Registers
+        string[] registerBuilder = new string[4];
+        for (var i = 0; i < console.CPU.Registers.Length; ++i)
+        {
+            string rgTop = $"V[{i.ToString("x")}]";
+            string rgBtm = $"0x{console.CPU.Registers[i].ToString("X2")}";
+            if (i < console.CPU.Registers.Length / 2 - 1 || i < console.CPU.Registers.Length - 1)
+            {
+                rgTop += " ";
+                rgBtm += " ";
+            }
+            if (i < console.CPU.Registers.Length / 2)
+            {
+                registerBuilder[0] += rgTop;
+                registerBuilder[1] += rgBtm;
+            }
+            else
+            {
+                registerBuilder[2] += rgTop;
+                registerBuilder[3] += rgBtm;
+            }
+        }
+        string registers = String.Join("\n", registerBuilder);
+        surface.WriteText(registers);
+        surface.Refresh();
     }
     /* Static Properties */
     private static char[] KeyInputs = new char[] {
